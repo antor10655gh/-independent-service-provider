78b990da7007b140e1db30c28f7102f0200df836
antor10655gh <creativefreelancerantor@gmail.com>
2022-04-18 14:39:09 +0600

    add blog2 and blog3

--- src/Pages/Blog/Blog2/Blog2.js
+++ src/Pages/Blog/Blog2/Blog2.js
@@ -1,8 +1,54 @@
 import React from "react";
 import "./Blog2.css";
+import blog2 from "../../../images/blog/blog2.jpg";
 
 const Blog2 = () => {
-  return <div></div>;
+  return (
+    <div className="container py-3 py-lg-5">
+      <header className="text-center mb-4 mb-lg-5">
+        <h2>Why are you using firebase ?</h2>
+        <div
+          style={{
+            width: "250px",
+            borderBottom: "3px solid orange",
+            margin: "0 auto",
+          }}
+        ></div>
+      </header>
+      <div className="row row-cols-1 row-cols-lg-2">
+        <div className="col">
+          <div className="blog1-banner">
+            <img className="img-fluid " src={blog2} alt="" />
+          </div>
+        </div>
+        <div className="col">
+          <div className="authorization my-4">
+            <h3>Firebase Benefit</h3>
+            <ul>
+              <li>Create Application without backend server.</li>
+              <li>No need extra money spent for backend server.</li>
+              <li>Sync real time data in the application.</li>
+              <li>Quick display data in the application.</li>
+              <li>Faster then any backend web services.</li>
+            </ul>
+          </div>
+          <div className="authorization my-4">
+            <h3>Some Option for Authentication</h3>
+            <ul>
+              <li>
+                Password-based authentication. Passwords are the most common
+                methods of authentication.
+              </li>
+              <li>Multi-factor authentication.</li>
+              <li>Certificate-based authentication.</li>
+              <li>Biometric authentication.</li>
+              <li>Token-based authentication.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog2;
--- src/Pages/Blog/Blog3/Blog3.js
+++ src/Pages/Blog/Blog3/Blog3.js
@@ -1,8 +1,52 @@
 import React from "react";
 import "./Blog3.css";
+import blog3 from "../../../images/blog/blog3.jpg";
 
 const Blog3 = () => {
-  return <div></div>;
+  return (
+    <div className="container py-3 py-lg-5">
+      <header className="text-center mb-4 mb-lg-5">
+        <h2>
+          What other services does firebase provide other than authentication ?
+        </h2>
+        <div
+          style={{
+            width: "450px",
+            borderBottom: "3px solid orange",
+            margin: "0 auto",
+          }}
+        ></div>
+      </header>
+      <div className="row row-cols-1 row-cols-lg-2">
+        <div className="col">
+          <div className="blog1-banner">
+            <img className="img-fluid " src={blog3} alt="" />
+          </div>
+        </div>
+        <div className="col">
+          <div className="authorization my-4">
+            <p>
+              Owned by Google, Firebase is a complete package of products that
+              allows to build web and mobile apps, improve the app quality and
+              help your clients grow their business.
+            </p>
+          </div>
+          <div className="authorization my-4">
+            <h3>
+              The dashboard displays your project's usage of Firebase-supported
+              infrastructure products, including the following:
+            </h3>
+            <ul>
+              <li>Cloud Storage.</li>
+              <li>Cloud Firestore.</li>
+              <li>Firebase Realtime Database.</li>
+              <li>Firebase Hosting.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog3;
