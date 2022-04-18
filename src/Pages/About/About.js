acb40db74b90e678e0e05a5ec22f45d96bc07613
antor10655gh <creativefreelancerantor@gmail.com>
2022-04-18 06:53:32 +0600

    about section added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,8 +1,40 @@
 import React from "react";
 import "./About.css";
+import aboutMe from "../../images/about-me.jpg";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div
+      style={{ background: "#e2e2e2" }}
+      className="container-fluid p-3 p-lg-5"
+    >
+      <div className="container p-5">
+        <div className="row row-cols-1 row-cols-lg-2">
+          <div className="col-lg-5">
+            <div className="about-banner">
+              <img className="img-fluid" src={aboutMe} alt="" />
+            </div>
+          </div>
+          <div className="col-lg-7">
+            <div className="about-details p-4">
+              <h1>About Me</h1>
+              <h3>
+                Designer & <span style={{ color: "orange" }}>Developer</span>
+              </h3>
+              <p>
+                It's Antor, Professional Web Developer. I love to write code,
+                it's my passion. I am here to help you, I am here to design your
+                web page.I can help you to describe yourself in the best way. No
+                matter how difficult your web page is, I'll find an easy way and
+                complete your work anyway. Stay connect with me……..😊
+              </p>
+              <button className="letsTalk-btn">let's talk</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
